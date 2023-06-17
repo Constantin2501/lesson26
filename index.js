a940391d36f5601cf4f2a76c3564705f50a09f14
Konstantin Krotov <ckwebmast@gmail.com>
2023-06-17 22:43:32 +0300

    complete lesson17

---
+++ index.js
@@ -0,0 +1,27 @@
+
+const getData = () => {
+    fetch('db.json')
+        .then(response => response.json())
+        .then(data => {
+            sendData(data)
+        })
+        .catch(error => {
+            console.log(error);
+        })
+}
+
+const sendData = (data) => {
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        body: JSON.stringify(data),
+          headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+          },
+    })
+    .catch(error => {
+        console.log(error);
+    })
+}
+
+
+getData()
